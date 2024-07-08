1448d5d88265c32f0599acf79baecc0b692c2fa0
Ayush Gajbhiye <140054627+Eirch-47@users.noreply.github.com>
2024-07-09 00:07:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
- streamlit run app1.py
+streamlit run app1.py
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
