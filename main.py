9b17f6c656ea71c1a949cb07ef567ba989ebaa90
Ayush Gajbhiye <140054627+Eirch-47@users.noreply.github.com>
2024-07-09 00:03:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import streamlit as st
+
 streamlit run app1.py
 
 def print_hi(name):
